ecf554d1e52017225e0bf6753f9fb6b4279d9a9c
Vinh <zinh@users.noreply.github.com>
2024-12-02 14:34:12 +0900

    fix bug

--- haskell/day01/app/Part02.hs
+++ haskell/day01/app/Part02.hs
@@ -3,6 +3,7 @@ module Main where
 import Data.List.Split (splitOn)
 import qualified Data.Map as Map
 import System.Environment (getArgs)
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
@@ -10,7 +11,7 @@ main = do
   contents <- readFile filename
   let (left, m) = foldl parse ([], Map.empty) (map parseLine $ lines contents)
       r = sum $ map (\n -> Map.findWithDefault 0 n m * n) left
-   in print (show r)
+   in print r
 
 parseLine :: String -> (Integer, Integer)
 parseLine line =
@@ -22,5 +23,5 @@ parse :: ([] Integer, Map.Map Integer Integer) -> (Integer, Integer) -> ([] Inte
 parse (lst, m) (left, right) = (left : lst, newM)
   where
     newM = case Map.lookup right m of
-      Nothing -> Map.fromList [(right, 1)]
+      Nothing -> Map.insert right 1 m
       Just v -> Map.insert right (v + 1) m

c7833ccf263870e36facc2c4f9040e8123d27972
Vinh Nguyen <zinh@users.noreply.github.com>
2025-01-15 14:41:04 +0900

    add test

--- haskell/day04/test/Spec.hs
+++ haskell/day04/test/Spec.hs
@@ -1,19 +1,20 @@
 module Main where
 
 import Test.HUnit
-import Day04.Grid (Grid, Position, inBounds, charAt)
+import Day04.Grid (Grid, Position, Direction, inBounds, charAt, checkXMAS)
 
 testGrid :: Grid
-testGrid = [ "ABC"
-          , "DEF"
-          , "GHI"
+testGrid = [ "XMAS"
+          , "XAMX"
+          , "XSAM"
+          , "XMAS"
           ]
 
 emptyGrid :: Grid
 emptyGrid = []
 
 tests :: Test
-tests = TestList [inBoundsTests, charAtTests]
+tests = TestList [inBoundsTests, charAtTests, checkXMASTests]
 
 inBoundsTests :: Test
 inBoundsTests = TestList
@@ -53,16 +54,16 @@ inBoundsTests = TestList
 charAtTests :: Test
 charAtTests = TestList
   [ TestLabel "Valid position middle" $ TestCase $ do
-      assertEqual "Should get 'E' at position (1,1)"
-        (Just 'E') (charAt testGrid (1,1))
+      assertEqual "Should get 'M' at position (0,1)"
+        (Just 'M') (charAt testGrid (0,1))
 
   , TestLabel "Valid position top-left" $ TestCase $ do
-      assertEqual "Should get 'A' at position (0,0)"
-        (Just 'A') (charAt testGrid (0,0))
+      assertEqual "Should get 'X' at position (0,0)"
+        (Just 'X') (charAt testGrid (0,0))
 
   , TestLabel "Valid position bottom-right" $ TestCase $ do
-      assertEqual "Should get 'I' at position (2,2)"
-        (Just 'I') (charAt testGrid (2,2))
+      assertEqual "Should get 'S' at position (3,3)"
+        (Just 'S') (charAt testGrid (3,3))
 
   , TestLabel "Out of bounds negative" $ TestCase $ do
       assertEqual "Should get Nothing for negative position"
@@ -70,11 +71,44 @@ charAtTests = TestList
 
   , TestLabel "Out of bounds too large" $ TestCase $ do
       assertEqual "Should get Nothing for position beyond grid"
-        Nothing (charAt testGrid (3,3))
+        Nothing (charAt testGrid (4,4))
 
   , TestLabel "Empty grid" $ TestCase $ do
       assertEqual "Should get Nothing for empty grid"
         Nothing (charAt emptyGrid (0,0))
+  ]
+
+checkXMASTests :: Test
+checkXMASTests = TestList
+  [ TestLabel "Horizontal XMAS" $ TestCase $ do
+      let direction = [(0,1), (0,2), (0,3)]
+      assertEqual "Should find XMAS horizontally"
+        True (checkXMAS testGrid (0,0) direction)
+
+  , TestLabel "Vertical XMAS" $ TestCase $ do
+      let direction = [(1,0), (2,0), (3,0)]
+      assertEqual "Should find XMAS vertically"
+        True (checkXMAS testGrid (0,0) direction)
+
+  , TestLabel "Not XMAS (wrong start)" $ TestCase $ do
+      let direction = [(0,1), (0,2), (0,3)]
+      assertEqual "Should not find XMAS when starting with wrong letter"
+        False (checkXMAS testGrid (0,1) direction)
+
+  , TestLabel "Not XMAS (incomplete pattern)" $ TestCase $ do
+      let direction = [(0,1), (0,2), (0,3)]
+      assertEqual "Should not find XMAS when pattern is incomplete"
+        False (checkXMAS testGrid (1,0) direction)
+
+  , TestLabel "Out of bounds" $ TestCase $ do
+      let direction = [(0,1), (0,2), (0,3)]
+      assertEqual "Should not find XMAS when pattern goes out of bounds"
+        False (checkXMAS testGrid (3,1) direction)
+
+  , TestLabel "Empty grid" $ TestCase $ do
+      let direction = [(0,1), (0,2), (0,3)]
+      assertEqual "Should not find XMAS in empty grid"
+        False (checkXMAS emptyGrid (0,0) direction)
   ]
 
 main :: IO ()

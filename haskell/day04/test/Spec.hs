f43a0bb4b2137d3f282274208f7569e527affb07
Vinh Nguyen <zinh@users.noreply.github.com>
2025-01-15 14:38:08 +0900

    add test

--- haskell/day04/test/Spec.hs
+++ haskell/day04/test/Spec.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Test.HUnit
-import Day04.Grid (Grid, Position, inBounds)
+import Day04.Grid (Grid, Position, inBounds, charAt)
 
 testGrid :: Grid
 testGrid = [ "ABC"
@@ -13,7 +13,10 @@ emptyGrid :: Grid
 emptyGrid = []
 
 tests :: Test
-tests = TestList
+tests = TestList [inBoundsTests, charAtTests]
+
+inBoundsTests :: Test
+inBoundsTests = TestList
   [ TestLabel "Valid position" $ TestCase $ do
       assertEqual "Position (1,1) should be in bounds"
         True (inBounds testGrid (1,1))
@@ -45,6 +48,33 @@ tests = TestList
   , TestLabel "Empty grid" $ TestCase $ do
       assertEqual "Any position in empty grid should be out of bounds"
         False (inBounds emptyGrid (0,0))
+  ]
+
+charAtTests :: Test
+charAtTests = TestList
+  [ TestLabel "Valid position middle" $ TestCase $ do
+      assertEqual "Should get 'E' at position (1,1)"
+        (Just 'E') (charAt testGrid (1,1))
+
+  , TestLabel "Valid position top-left" $ TestCase $ do
+      assertEqual "Should get 'A' at position (0,0)"
+        (Just 'A') (charAt testGrid (0,0))
+
+  , TestLabel "Valid position bottom-right" $ TestCase $ do
+      assertEqual "Should get 'I' at position (2,2)"
+        (Just 'I') (charAt testGrid (2,2))
+
+  , TestLabel "Out of bounds negative" $ TestCase $ do
+      assertEqual "Should get Nothing for negative position"
+        Nothing (charAt testGrid (-1,0))
+
+  , TestLabel "Out of bounds too large" $ TestCase $ do
+      assertEqual "Should get Nothing for position beyond grid"
+        Nothing (charAt testGrid (3,3))
+
+  , TestLabel "Empty grid" $ TestCase $ do
+      assertEqual "Should get Nothing for empty grid"
+        Nothing (charAt emptyGrid (0,0))
   ]
 
 main :: IO ()
